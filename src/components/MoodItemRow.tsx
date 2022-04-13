083c93cf5dfb6b70703bec2a1d4b445755b1964c
mohammedsadeghi <sadeghimohammad4488@gmail.com>
2022-04-14 00:26:07 +0430

    gesture has been added

--- src/components/MoodItemRow.tsx
+++ src/components/MoodItemRow.tsx
@@ -5,30 +5,89 @@ import {MoodOptionWithTimestamp} from '../Types';
 import {theme} from '../Theme';
 import {useContext} from 'react';
 import {AppContext} from '../App.provider';
+import {
+  PanGestureHandler,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
+import {useAnimatedGestureHandler, withTiming} from 'react-native-reanimated';
+import Reanimated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
+import {PanGestureHandlerGestureEvent} from 'react-native-gesture-handler';
+import {runOnJS} from 'react-native-reanimated';
 
 type MoodItemRowProps = {
   item: MoodOptionWithTimestamp;
 };
 
 export const MoodItemRow: React.FC<MoodItemRowProps> = ({item}) => {
+  const offset = useSharedValue(0);
   const appContext = useContext(AppContext);
   const handleDelete = React.useCallback(() => {
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
     appContext.handleDeleteMood(item);
   }, [item, appContext]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{translateX: offset.value}],
+  }));
+
+  const removeWithDelay = React.useCallback(() => {
+    setTimeout(() => {
+      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+      appContext.handleDeleteMood(item);
+    }, 250);
+  }, [appContext, item]);
+
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    {shouldRemove: boolean}
+  >(
+    {
+      onActive: (event, ctx) => {
+        const xVal = Math.floor(event.translationX);
+        offset.value = xVal;
+
+        // use Absolute value so the user could swipe either left or right
+        if (Math.abs(xVal) <= 100) {
+          ctx.shouldRemove = false;
+        } else {
+          ctx.shouldRemove = true;
+        }
+      },
+      onEnd: (_, ctx) => {
+        if (ctx.shouldRemove) {
+          // if the item should be removed, animate it off the screen first
+          offset.value = withTiming(Math.sign(offset.value) * 2000);
+
+          // then trigger the remove mood item with a small delay
+          runOnJS(removeWithDelay)();
+        } else {
+          // otherwise, animate the item back to the start
+          offset.value = withTiming(0);
+        }
+      },
+    },
+    [],
+  );
   return (
-    <View style={styles.moodItem}>
-      <View style={styles.iconAndDescription}>
-        <Text style={styles.moodValue}>{item.mood.emoji}</Text>
-        <Text style={styles.moodDescription}>{item.mood.description}</Text>
-      </View>
-      <Text style={styles.moodDate}>
-        {format(new Date(item.timestamp), "dd MMM, yyyy 'at' h:mmaaa")}
-      </Text>
-      <Pressable hitSlop={16} onPress={handleDelete}>
-        <Text style={styles.deleteText}>Delete</Text>
-      </Pressable>
-    </View>
+    <GestureHandlerRootView>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Reanimated.View style={[styles.moodItem, animatedStyle]}>
+          <View style={styles.iconAndDescription}>
+            <Text style={styles.moodValue}>{item.mood.emoji}</Text>
+            <Text style={styles.moodDescription}>{item.mood.description}</Text>
+          </View>
+          <Text style={styles.moodDate}>
+            {format(new Date(item.timestamp), "dd MMM, yyyy 'at' h:mmaaa")}
+          </Text>
+          <Pressable hitSlop={16} onPress={handleDelete}>
+            <Text style={styles.deleteText}>Delete</Text>
+          </Pressable>
+        </Reanimated.View>
+      </PanGestureHandler>
+    </GestureHandlerRootView>
   );
 };
 
